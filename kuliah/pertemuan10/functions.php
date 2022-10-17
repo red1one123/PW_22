6c324578aa8bef0c7ebf0aab9b7f8691eeb40eaa
Ridwan <112155562+red1one123@users.noreply.github.com>
2022-10-17 09:14:16 +0700

    update

---
+++ kuliah/pertemuan10/functions.php
@@ -0,0 +1,22 @@
+<?php
+function koneksi()
+{
+  return mysqli_connect('localhost', 'root', '', 'pw-uika');
+}
+
+function Query($query)
+{
+  $conn = koneksi();
+  $result = mysqli_query($conn, $query);
+
+  //jika hasilnya haya satu
+  if (mysqli_num_rows($result) == 1) {
+    return mysqli_fetch_assoc($result);
+  }
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+  return $rows;
+}
