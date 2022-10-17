fd0cdc55de8cebea4fd7b3624a30c03c4d8c0895
Ridwan <112155562+red1one123@users.noreply.github.com>
2022-10-17 14:50:40 +0700

    tambah data

---
+++ kuliah/pertemuan10/tambah.php
@@ -0,0 +1,66 @@
+<?php
+require 'functions.php';
+//cek apakah tombol tambah sudah ditekan
+if (isset($_POST['Tambah'])) {
+  var_dump($_POST);
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tambah Data Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Tambah Data Mahasiswa</h3>
+  <form action="" method="POST"></form>
+  <ul>
+    <li>
+      <label>
+        Nama :
+        <input type="text" name="nama" autofocus>
+      </label>
+    <li>
+      <label>
+        Nrp :
+        <input type="text" name="nrp">
+      </label>
+    <li>
+      <label>
+        Email :
+        <input type="text" name="email">
+      </label>
+    </li>
+    <li>
+      <label>
+        Jurusan :
+        <input type="text" name="jurusan">
+      </label>
+    </li>
+    <li>
+      <label>
+        Gambar :
+        <input type="text" name="gambar">
+      </label>
+    </li>
+    <li>
+      <button type="submit" name="tambah">Tambah Data</button>
+    </li>
+    </li>
+    </li>
+    </li>
+    </li>
+  </ul>
+
+
+
+</body>
+
+</html>
