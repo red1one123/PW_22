9a947d8a96c0d03a994b09109af95cea543f2ec6
Ridwan <112155562+red1one123@users.noreply.github.com>
2022-10-18 16:33:45 +0700

    tambah data

---
+++ kuliah/pertemuan11/hapus.php
@@ -0,0 +1,14 @@
+<?php
+require 'functions.php';
+//mengambil dari url
+$id = $_GET['id'];
+
+
+if (hapus($id) > 0) {
+  echo "<script>
+    alert('data berhasil dihapus');
+    document.location.href = 'index.php';
+        </script>";
+} else {
+  echo "data gagal ditambahkan!";
+}
