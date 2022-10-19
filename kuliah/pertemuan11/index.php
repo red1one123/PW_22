61e62e5d81cad40d6483684fb5197e60310abae8
Ridwan <112155562+red1one123@users.noreply.github.com>
2022-10-19 10:57:51 +0700

    baru lagi

--- kuliah/pertemuan11/index.php
+++ kuliah/pertemuan11/index.php
@@ -1,6 +1,11 @@
 <?php
 require 'functions.php';
 $mahasiswa = Query("SELECT * FROM mahasiswa");
+//ketika tombol cari di klik
+if (isset($_POST['cari'])) {
+  $mahasiswa = cari($_POST['keyword']);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -15,8 +20,15 @@ $mahasiswa = Query("SELECT * FROM mahasiswa");
 
 <body>
   <h3>Daftar Mahasiswa</h3>
+
   <a href="tambah.php">Tambah Data Mahasiswa</a>
   <br></br>
+  <form action="" method="POST">
+    <input type="text" name="keyword" size="36" placeholder="masukan keyword pencarian..
+    " autocomplete="off" autofocus>
+    <button type="submit" name="cari">cari!</button>
+  </form>
+  <br>
 
   <table border="1" cellpadding="10" cellspacing="0">
     <tr>
@@ -25,6 +37,15 @@ $mahasiswa = Query("SELECT * FROM mahasiswa");
       <th>Nama</th>
       <th>Aksi</th>
     </tr>
+
+    <?php if (empty($mahasiswa)) : ?>
+      <tr>
+        <td colspan="4">
+          <p style="color: red; font-style:italic">data mahasiswa tidak ditemukan!</p>
+        </td>
+      </tr>
+    <?php endif; ?>
+
     <?php $i = 1; ?>
     <?php foreach ($mahasiswa as $mhs) : ?>
       <tr>
