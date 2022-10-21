61342207df00d38fa02a3fde12ac813a3620b36f
Ridwan <112155562+red1one123@users.noreply.github.com>
2022-10-21 13:16:34 +0700

    tambah latihan

---
+++ kuliah/pertemuan13/ajax/ajax_cari.php
@@ -0,0 +1,34 @@
+<?php
+require '../functions.php';
+$mahasiswa = cari($_GET['keyword']);
+?>
+
+<table border="1" cellpadding="10" cellspacing="0">
+  <tr>
+    <th>#</th>
+    <th>Gambar</th>
+    <th>Nama</th>
+    <th>Aksi</th>
+  </tr>
+
+  <?php if (empty($mahasiswa)) : ?>
+    <tr>
+      <td colspan="4">
+        <p style="color: red; font-style:italic">data mahasiswa tidak ditemukan!</p>
+      </td>
+    </tr>
+  <?php endif; ?>
+
+  <?php $i = 1; ?>
+  <?php foreach ($mahasiswa as $mhs) : ?>
+    <tr>
+      <td><?= $i++;  ?></td>
+      <td><img src="img/<?= $mhs['gambar']; ?>" width="60"></td>
+      <td><?= $mhs['nama']; ?></td>
+      <td>
+        <a href="detail.php?id=<?= $mhs['id']; ?>">Lihat Detail</a>
+
+      </td>
+    </tr>
+  <?php endforeach; ?>
+</table>
